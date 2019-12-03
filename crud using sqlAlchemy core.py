c66d7a83a26cadb0f73986c3a681a1170907b112
Prajakta <prajaktavpendse@gmail.com>
2019-12-02 22:48:42 -0500

    Task: Customer id and order id

--- crud using sqlAlchemy core.py
+++ crud using sqlAlchemy core.py
@@ -100,3 +100,140 @@ r = conn.execute(ins,
     town = 'Langdale'
 )
 print(r.inserted_primary_key)
+
+r = conn.execute(ins, [
+        {
+            "first_name": "John",
+            "last_name": "Lara",
+            "username": "johnlara",
+            "email":"johnlara@mail.com",
+            "address": "3073 Derek Drive",
+            "town": "Norfolk"
+        },
+        {
+            "first_name": "Sarah",
+            "last_name": "Tomlin",
+            "username": "sarahtomlin",
+            "email":"sarahtomlin@mail.com",
+            "address": "3572 Poplar Avenue",
+            "town": "Norfolk"
+        },
+        {
+            "first_name": "Pablo",
+            "last_name": "Gibson",
+            "username": "pablogibson",
+            "email":"pablogibson@mail.com",
+            "address": "3494 Murry Street",
+            "town": "Peterbrugh"
+        },
+        {
+            "first_name": "Pablo",
+            "last_name": "Lewis",
+            "username": "pablolewis",
+            "email":"pablolewis@mail.com",
+            "address": "3282 Jerry Toth Drive",
+            "town": "Peterbrugh"
+        },
+    ])
+
+print(r.rowcount)
+
+
+items_list = [
+    {
+        "name":"Chair",
+        "cost_price": 9.21,
+        "selling_price": 10.81,
+        "quantity": 10
+    },
+    {
+        "name":"Pen",
+        "cost_price": 3.45,
+        "selling_price": 4.51,
+        "quantity": 3
+    },
+    {
+        "name":"Headphone",
+        "cost_price": 15.52,
+        "selling_price": 16.81,
+        "quantity": 50
+    },
+    {
+        "name":"Travel Bag",
+        "cost_price": 20.1,
+        "selling_price": 24.21,
+        "quantity": 50
+    },
+    {
+        "name":"Keyboard",
+        "cost_price": 20.12,
+        "selling_price": 22.11,
+        "quantity": 50
+    },
+    {
+        "name":"Monitor",
+        "cost_price": 200.14,
+        "selling_price": 212.89,
+        "quantity": 50
+    },
+    {
+        "name":"Watch",
+        "cost_price": 100.58,
+        "selling_price": 104.41,
+        "quantity": 50
+    },
+    {
+        "name":"Water Bottle",
+        "cost_price": 20.89,
+        "selling_price": 25.00,
+        "quantity": 50
+    },
+]
+
+order_list = [
+    {
+        "customer_id": 1
+    },
+    {
+        "customer_id": 1
+    }
+]
+
+order_line_list = [
+    {
+        "order_id": 1,
+        "item_id": 1,
+        "quantity": 5
+    },
+    {
+        "order_id": 1,
+        "item_id": 2,
+        "quantity": 2
+    },
+    {
+        "order_id": 1,
+        "item_id": 3,
+        "quantity": 1
+    },
+    {
+        "order_id": 2,
+        "item_id": 1,
+        "quantity": 5
+    },
+    {
+        "order_id": 2,
+        "item_id": 2,
+        "quantity": 4
+    },
+]
+
+r = conn.execute(insert(items), items_list)
+print(r.rowcount)
+r = conn.execute(insert(orders), order_list)
+print(r.rowcount)
+r = conn.execute(insert(order_lines), order_line_list)
+print(r.rowcount)
+
+
+s = customers.select()
+print(str(s))
