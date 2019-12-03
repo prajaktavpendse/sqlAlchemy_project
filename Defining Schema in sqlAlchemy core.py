cb5a10faed6524597b6eb45ff88017b1da2d54bc
Prajakta <prajaktavpendse@gmail.com>
2019-12-02 21:59:18 -0500

    Task: Added tables

--- Defining Schema in sqlAlchemy core.py
+++ Defining Schema in sqlAlchemy core.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, Text, DateTime, Boolean, ForeignKey
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, Text, DateTime, Boolean, ForeignKey,Numeric, CheckConstraint
+
+from datetime import datetime
 
 metadata = MetaData()
 
@@ -30,3 +32,50 @@ print(posts.primary_key)     # returns the primary key of the column
 print(posts.metadata)        # get the MetaData object from the table
 print(posts.columns.post_title.name)     # returns the name of the column
 print(posts.columns.post_title.type)     # returns the type of the column
+
+engine = create_engine("sqlite:///sqlalchemy_tuts.db")
+
+engine.connect()
+
+print(engine)
+
+customers = Table('customers', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('first_name', String(100), nullable=False),
+    Column('last_name', String(100), nullable=False),
+    Column('username', String(50), nullable=False),
+    Column('email', String(200), nullable=False),
+    Column('address', String(200), nullable=False),
+    Column('town', String(50), nullable=False),
+    Column('created_on', DateTime(), default=datetime.now),
+    Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+)
+
+
+items = Table('items', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(200), nullable=False),
+    Column('cost_price', Numeric(10, 2), nullable=False),
+    Column('selling_price', Numeric(10, 2),  nullable=False),
+    Column('quantity', Integer(), nullable=False),
+    CheckConstraint('quantity > 0', name='quantity_check')
+)
+
+
+orders = Table('orders', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('customer_id', ForeignKey('customers.id')),
+    Column('date_placed', DateTime(), default=datetime.now),
+    Column('date_shipped', DateTime())
+)
+
+
+order_lines = Table('order_lines', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('order_id', ForeignKey('orders.id')),
+    Column('item_id', ForeignKey('items.id')),
+    Column('quantity', Integer())
+)
+
+
+metadata.create_all(engine)
--- Installing and connecting to db.py
+++ Installing and connecting to db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://root:pass@localhost/mydb")
-
+engine = create_engine("sqlite:///sqlalchemy_tuts.db")
 engine.connect()
 
 print(engine)
+
